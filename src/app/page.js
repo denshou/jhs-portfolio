56aaa7825357a88292dc37e37907374ab77ff977
denshou <132555762+denshou@users.noreply.github.com>
2024-06-19 01:58:56 +0900

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <p>JHS-Portfolio</p>
+        <div>
+          <p>About me</p>
+          <p>Skills</p>
+          <p>Archive</p>
+          <p>Projects</p>
+        </div>
+      </div>
+      <div>
+        <p>프론트 엔드 개발자 포트폴리오</p>
+        <hr />
+        <p>안녕하세요. </p>
+        <p>프론트 엔드 개발자 정현승입니다.</p>
+      </div>
+      <div>
+        <p>ABOUT ME</p>
+        <hr />
+        <div>
+          <div>
+            <p>이름</p>
+            <p>정현승</p>
+          </div>
+          <div>
+            <p>생년월일</p>
+            <p>1998.09.01</p>
+          </div>
+          <div>
+            <p>주소지</p>
+            <p>경기도 수원시</p>
+          </div>
+          <div>
+            <p>연락처</p>
+            <p>010-2994-8697</p>
+          </div>
+          <div>
+            <p>이메일</p>
+            <p>zecaumr@naver.com</p>
+          </div>
+          <div>
+            <p>학력</p>
+            <p>세종대학교(전자정보통신공학과)</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>SKILLS</p>
+      </div>
+      <div>
+        <p>ARCHIVE</p>
+      </div>
+      <div>
+        <p>PROJECTS</p>
+      </div>
+      <div>
+        <p>footer</p>
+      </div>
+    </>
+  );
+}
