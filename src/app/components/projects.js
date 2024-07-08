b3de965ae39993013872518d96e82db5c735aebb
denshou <132555762+denshou@users.noreply.github.com>
2024-07-09 02:20:36 +0900

    내용 채우기 진행중

--- src/app/components/projects.js
+++ src/app/components/projects.js
@@ -5,11 +5,73 @@ export default function Projects() {
         <div className="section-fifth">
           <p className="section-title">PROJECTS</p>
           <div className="pj-container">
-            <p className="pj-title">NUWA</p>
-            <p className="pj-span">기간</p>
+            <p className="pj-title">NUWA(실시간 협업툴)</p>
+            <p className="pj-span">2024.01~2024.04(7인 프로젝트)</p>
             <div className="pj-detail">
-              <div>사진</div>
-              <div>설명</div>
+              <div className="pj-img">
+                <img src="/nuwa1.png" alt="" />
+              </div>
+              <div className="pj-description">
+                <p className="main-des">
+                  실시간으로 소통할 수 있는 협업툴입니다. 기존 앱들과는 다르게
+                  필요한 기능만을 사용해 간편하게 이용할 수 있도록 개발했습니다.
+                </p>
+                <div className="sub-des">
+                  <div className="label">주요 기능</div>
+                  <div>
+                    <p>
+                      워크스페이스를 생성하고 초대링크 생성 및 이메일로
+                      전송합니다.
+                    </p>
+                    <p>
+                      1:1 채팅, 그룹 채팅이 실시간으로 가능하고 SSE를 통해
+                      알림이 전송됩니다.
+                    </p>
+                    <p> 채팅 별 이미지, 파일 업로드가 가능합니다.</p>
+                    <p>
+                      메세지의 마지막 시간에 따라 채팅방 조회가 가능하고 내가
+                      보낸 메세지 수가 많은 인원을 대쉬보드에서 보여줍니다.
+                    </p>
+                  </div>
+                </div>
+                <div className="sub-des">
+                  <div className="label">GitHub</div>
+                  <p>github.com/NU-WA-Project</p>
+                </div>
+                <div className="sub-des">
+                  <div className="label">URL</div>
+                  <p>nu-wa.online</p>
+                </div>
+                <div className="sub-des">
+                  <div className="label">Frontend</div>
+                  <p>nu-wa.online</p>
+                </div>
+                <div className="sub-des">
+                  <div className="label">Backend</div>
+                  <p>nu-wa.online</p>
+                </div>
+                <div className="sub-des">
+                  <div className="label">Database</div>
+                  <p>nu-wa.online</p>
+                </div>
+                <div className="sub-des">
+                  <div className="label">Deployment</div>
+                  <p>nu-wa.online</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="pj-container">
+            <p className="pj-title">NUWA(실시간 협업툴)</p>
+            <p className="pj-span">2024.01~2024.04(7인 프로젝트)</p>
+            <div className="pj-detail">
+              <div className="pj-img">
+                <img src="/nuwa1.png" alt="" />
+              </div>
+              <div className="pj-description">
+                <p>설명</p>
+                <p>주요 기능</p>
+              </div>
             </div>
           </div>
         </div>
